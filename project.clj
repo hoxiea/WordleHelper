8f32c79b781a805342302350699f7f487c6e1404
hoxie <hoxiea@gmail.com>
2022-04-29 21:26:10 -0400

    Add lein count for easy LOC counting

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-auto "0.1.3"]]
+  :plugins [[lein-auto "0.1.3"]
+            [lein-count "1.0.9"]]
   :dependencies [[clojure-term-colors "0.1.0"]
                  [org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot wordle-helper.core
